97f1f30b1bf5eb0c90f996089a325f959525e3e4
jachoo <1050784+jachoo@users.noreply.github.com>
2021-05-14 18:45:49 +0200

    Version bump (0.2.2)

--- hopt/__init__.py
+++ hopt/__init__.py
@@ -3,6 +3,6 @@ from .hopt import Parameters, search, crossval_search
 from . import randoms
 
 __author__ = 'Adam Brzeski'
-__version__ = '0.1.0'
+__version__ = '0.2.2'
 
 __all__ = [Parameters, search, crossval_search, randoms]
